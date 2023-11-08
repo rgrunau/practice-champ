c9516df47e001b613e47576f5c3d3f3c396133cf
Robert Grunau <89792978+rgrunau@users.noreply.github.com>
2023-11-08 11:39:04 -0800

    still messing around a bit

---
+++ src/app/new-user/page.tsx
@@ -0,0 +1,9 @@
+export default function NewUserPage() {
+  return (
+    <main className="text-slate-50">
+      <div>
+        <h1>New User</h1>
+      </div>
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,26 @@
 import Image from "next/image";
+import { auth } from "@clerk/nextjs";
 import { metadata } from "./layout";
+import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth();
+  const href = userId ? "/studio" : "/sign-in";
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+    <main className="flex w-full max-w-5xl mx-auto min-h-screen flex-col items-start justify-center text-left p-12 md:p-24">
       <div>
-        <h1 className="text-slate-50 text-4xl">Practice Champ</h1>
+        <h1 className="text-slate-50 text-7xl">Practice Champ</h1>
       </div>
-      <div className="text-slate-200">
+      <div className="w-full max-w-sm text-slate-200 text-2xl mt-3">
         <p>{metadata.description}</p>
+      </div>
+      <div className="flex items-center justify-start mt-4">
+        <Link
+          href={href}
+          className="text-slate-50 text-xl bg-blue-600 rounded-md px-2 py-4"
+        >
+          Log a Practice Session
+        </Link>
       </div>
     </main>
   );
